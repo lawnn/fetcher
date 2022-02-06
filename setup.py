8293d5854b2c610eacef2675c2afc58574abaa26
lawn <bloom.all.over.12@gmail.com>
2022-02-06 16:23:18 +0900

    v0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='wrappy',
     packages=['wrappy'],
-    version='0.0.6',
+    version='0.1.0',
     author='lawn',
     url='https://github.com/lawnn/wrappy.git',
     install_requires=['requests', 'asyncio', 'pybotters']
