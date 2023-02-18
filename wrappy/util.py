37c74f4f8aab223a0352cae58ab9c52c05b854e4
lawnn <bloom.all.over.12@gmail.com>
2023-02-18 23:10:52 +0900

    Update bitflyer get trades

--- wrappy/util.py
+++ wrappy/util.py
@@ -338,65 +338,160 @@ class Util:
         print(f'elapsed time: {time.time() - start:.2f}sec')
 
     @classmethod
-    def bf_get_trades(cls, st_date: str, symbol: str = 'FX_BTC_JPY', output_dir: str = None) -> None:
+    def bf_get_trades(cls, st_date: str, end_date: str, symbol: str = 'FX_BTC_JPY', output_dir: str = None,
+                      request_interval: float = 0.6) -> None:
         """ example
-        bf_get_trades('2021/09/01')
+        bf_get_trades('2021/09/01 00:00:00')
         :param output_dir: str
-        :param st_date: 2021/09/01
-        :param symbol: FX_BTC_JPY, BTC_JPY, ETH_JPY
+        :param st_date: 2021/09/01 00:00:00     # start date
+        :param end_date: 2021/09/10 00:00:00    # end date
+        :param symbol: FX_BTC_JPY, BTC_JPY, ETH_JPY etc...
         :return:
         """
+
+        def bf_search_id(target_dt, count_id=14000):
+            def bf_date_to_dt():
+                try:
+                    return datetime.strptime(response[0]["exec_date"] + "+0000", "%Y-%m-%dT%H:%M:%S.%f" + "%z")
+                except ValueError:
+                    return datetime.strptime(response[0]["exec_date"] + "+0000", "%Y-%m-%dT%H:%M:%S" + "%z")
+                except KeyError as  e:
+                    print(response)
+                    print(e)
+
+            target_dt = target_dt.replace('/', '-')
+            dt = datetime.strptime(target_dt + "+0000", "%Y-%m-%d %H:%M:%S" + "%z")
+            target_date = dt.astimezone(utc)
+
+            # target_dateと現在時刻のおおまかな時間差
+            hours = int((time.time() - target_date.timestamp()) / 3600) + 1
+
+            # 最新約定履歴ID取得
+            params = dict(product_code=symbol, count=500)
+            response = requests.get("https://api.bitflyer.com/v1/getexecutions", params=params).json()
+            end_id = response[0]["id"]
+
+            # 二分探索開始ID設定
+            start_id = end_id - hours * count_id  # 平均count_id件/時間と仮定する
+
+            # start_idの約定履歴を取得
+            params["before"] = start_id + 1
+            response = requests.get("https://api.bitflyer.com/v1/getexecutions", params=params).json()
+
+            # start_idの約定日時(exec_date)をdatetime(UTC)変換
+
+            start_date = bf_date_to_dt()
+
+            # target_dateより過去日時になるまでstart_idをずらす
+            while start_date > target_date:
+                # 1時間分(count_id件)idを差し引いて約定履歴を再取得
+                start_id -= count_id
+                params["before"] = start_id + 1
+                response = requests.get("https://api.bitflyer.com/v1/getexecutions", params=params).json()
+
+                # start_idの約定日時(exec_date)をdatetime(UTC)変換
+                start_date = bf_date_to_dt()
+                time.sleep(request_interval)
+
+            # 検索範囲が500件以下になるまで絞り込み
+            while end_id - start_id > 500:
+                # idの中央値を算出
+                mid_id = int((start_id + end_id) / 2)
+
+                # 中央値の約定履歴を取得
+                params["before"] = mid_id + 1
+                response = requests.get("https://api.bitflyer.com/v1/getexecutions", params=params).json()
+
+                # 中央値の約定日時(exec_date)をdatetime(UTC)変換
+                mid_date = bf_date_to_dt()
+
+                # mid_dateがtarget_dateの左右どちらかチェック
+                if mid_date < target_date:
+                    # target_dateは中央値よりも右(未来)のため、start_idをずらす
+                    start_id = mid_id
+                else:
+                    # target_dateは中央値よりも左(過去)のため、end_idをずらす
+                    end_id = mid_id
+                time.sleep(request_interval)
+
+            # 絞り込んだend_idまでの約定履歴を取得
+            params["before"] = end_id + 1
+            response = requests.get("https://api.bitflyer.com/v1/getexecutions", params=params).json()
+            time.sleep(request_interval)
+
+            # 約定履歴リストは新→古順のため、反転する
+            execs = response[::-1]
+
+            # 指定時刻後の最初のIDを検索
+            for ex in execs:
+                try:
+                    ex_date = datetime.strptime(ex["exec_date"] + "+0000", "%Y-%m-%dT%H:%M:%S.%f" + "%z")
+                except ValueError:
+                    ex_date = datetime.strptime(ex["exec_date"] + "+0000", "%Y-%m-%dT%H:%M:%S" + "%z")
+                if target_date < ex_date:
+                    # 検索結果出力
+                    return ex["id"]
 
         start = time.time()
 
         if output_dir is None:
-            output_dir = f'./bitflyer/{symbol}/trades/'
+            output_dir = f'./bitflyer/{symbol}/trades'
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
-        path = f'{output_dir}/{symbol}.csv'
+        start_id = bf_search_id(st_date)
+        print(f'[start date] {st_date} [target id] {start_id}')
 
-        if os.path.isfile(path):
-            print(f"Found old data --> {path}\nDifference update...\n")
-            df_old = pd.read_csv(path, index_col='exec_date', parse_dates=True)
-            start_date = df_old.index[-1]
-        else:
-            df_old = None
-            st_date = st_date.replace('/', '-')
-            start_date = datetime.strptime(st_date, '%Y-%m-%d %H:%M:%S')
+        end_id = bf_search_id(end_date)
+        print(f'[end date] {end_date} [target id] {end_id}')
 
-        print(f'Until  --> {start_date}')
+        params = dict(product_code=symbol, count=500)
+        params["before"] = end_id
+        response = requests.get('https://api.bitflyer.com/v1/getexecutions', params=params).json()
+        end_base_id = end_id = response[0]['id']
+        new_percentage = percentage = 0
+        time.sleep(request_interval)
 
-        r = requests.get('https://api.bitflyer.com/v1/getexecutions', params=dict(product_code=symbol, count=500))
-        data = r.json()
-        df = pd.DataFrame(data[::-1]).set_index('exec_date')
-        last_date = pd.to_datetime(df.index, utc=True).tz_localize(None)[-1]
-        ID = data[-1]['id']
-        count = 0
+        while start_id <= end_id:
+            params["before"] = end_id
+            temp_r = requests.get('https://api.bitflyer.com/v1/getexecutions', params=params).json()
+            try:
+                end_id = temp_r[-1]['id']
+            except KeyError:
+                print(temp_r)
+                break
 
-        while start_date <= last_date:
-            if count % 500 == 0:
-                print(last_date)
-            count += 1
-            temp_r = requests.get('https://api.bitflyer.com/v1/getexecutions',
-                                  params=dict(product_code=symbol, count=500, before=ID))
-            temp_data = temp_r.json()
-            temp_df = pd.DataFrame(temp_data[::-1]).set_index('exec_date')
-            last_date = pd.to_datetime(temp_df.index, utc=True).tz_localize(None)[-1]
-            ID = int(temp_data[-1]['id'])
-            df = pd.concat([temp_df, df])
-            time.sleep(0.59)
+            if response[-1]["exec_date"][:13] != temp_r[-1]["exec_date"][:13]:
+                new_percentage = (end_base_id - end_id) / (end_base_id - start_id) * 100
+                progress_time = temp_r[-1]["exec_date"][:19]
+                progress_time = progress_time.replace("T", " ")
 
-        df.index = pd.to_datetime(df.index, utc=True).tz_localize(None)
-        df = df.astype({'price': 'float', 'size': 'float'})
+            if percentage != new_percentage:
+                percentage = new_percentage
+                print("\r", f"Progress: {percentage:.1f}%   Current date: {progress_time}", end="")
 
-        if os.path.isfile(path):
-            df = pd.concat([df_old, df])
-            df = df.drop_duplicates()
+            if response[-1]["exec_date"][:10] != temp_r[-1]["exec_date"][:10]:
+                response.extend(temp_r)
+                df = pd.DataFrame(response)
+                df = df[::-1]
+                dt = datetime.strptime(response[-1]["exec_date"][:10], '%Y-%m-%d') + timedelta(days=1)
+                dt = dt.strftime('%Y-%m-%d')
+                path = f'{output_dir}/{dt}.csv'
+                path = path.replace('//', '/')
+                print(f'   Output --> {path}')
+                df.to_csv(path)
+            response.extend(temp_r)
+            time.sleep(request_interval)
 
+        df = pd.DataFrame(response)
+        df = df[::-1]
+        dt = datetime.strptime(response[-1]["exec_date"][:10], '%Y-%m-%d') + timedelta(days=1)
+        dt = dt.strftime('%Y-%m-%d')
+        path = f'{output_dir}/{dt}-all.csv'
+        path = path.replace('//', '/')
+        print(f'Output --> {path}')
         df.to_csv(path)
 
-        print(f'Output --> {path}')
         print(f'elapsed time: {(time.time() - start) / 60:.2f}min')
 
     @classmethod
