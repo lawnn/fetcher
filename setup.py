10e6e2bea74b6d5ac33765bab294be75ce7c9b64
lawnn <bloom.all.over.12@gmail.com>
2023-03-19 01:43:27 +0900

    Add binance

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='fetcher',
     packages=['fetcher'],
-    version='0.1.0',
+    version='0.2.0',
     author='lawn',
     url='https://github.com/lawnn/fetcher.git',
     install_requires=['requests', 'asyncio', 'matplotlib', 'pandas', 'numpy', 'pytz']
--- wrappy/__init__.py
+++ wrappy/__init__.py
@@ -1,2 +1,3 @@
+from .binance import binance_get_OI, binance_get_buy_sell_vol, binance_get_1st_id, binance_get_trades, binance_fetch_trades
 from .bitflyer import bf_get_historical, bf_get_trades
 from .util import Util
