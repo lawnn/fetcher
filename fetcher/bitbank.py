89ca568954138727e837428ed92af20aadc0e4c1
lawnn <bloom.all.over.12@gmail.com>
2023-03-19 11:40:13 +0900

    bitbank: Add trades to historical

--- fetcher/bitbank.py
+++ fetcher/bitbank.py
@@ -14,3 +14,22 @@ def bitbank_get_trades(st_date: str, symbol: str = "btc_jpy", output_dir: str = 
         os.makedirs(output_dir)
 
     pl.DataFrame(r["data"]["transactions"]).write_csv(f"{output_dir}/{dt}.csv")
+
+
+def bitbank_trades_to_historical(path: str, period: str = '1s') -> pl.DataFrame:
+    return (
+            pl.scan_csv(f"{path}")
+                .with_columns(
+                (pl.col("executed_at") * 1_000)
+                    .cast(pl.Datetime(time_unit='us'))
+                    .alias('datetime')
+                )
+                .groupby_dynamic('datetime', every=period)
+                .agg([
+                pl.col("price").first().alias('open'),
+                pl.col("price").max().alias('high'),
+                pl.col("price").min().alias('low'),
+                pl.col("price").last().alias('close'),
+                pl.col("amount").sum().alias('volume'),
+                ])
+            ).collect()
