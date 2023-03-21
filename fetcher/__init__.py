33c59971c47868d1c44452cced9a4bca17ec7e96
lawnn <bloom.all.over.12@gmail.com>
2023-03-21 18:42:16 +0900

    util: Add str_to_datetime

--- fetcher/__init__.py
+++ fetcher/__init__.py
@@ -3,4 +3,4 @@ from .binance import binance_get_OI, binance_get_buy_sell_vol, binance_get_1st_i
 from .bitfinex import  bitfinex_get_trades
 from .bitflyer import bf_get_historical, bf_get_trades, bf_trades_to_historical
 from .gmo import gmo_get_historical, gmo_get_trades, gmo_trades_to_historical
-from .util import datetime_to_ms, datetime_to_timestamp, np_shift, resample_ohlc, trades_to_historical, df_list
+from .util import datetime_to_ms, datetime_to_timestamp, str_to_datetime, np_shift, resample_ohlc, trades_to_historical, df_list
