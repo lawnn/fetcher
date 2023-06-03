8f7e6819446e5b0f52ea8a7276f939f3284df271
lawnn <bloom.all.over.12@gmail.com>
2023-06-04 01:25:01 +0900

    v7.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='fetcher',
     packages=['fetcher'],
-    version='7.2',
+    version='7.3',
     author='lawn',
     url='https://github.com/lawnn/fetcher.git',
     install_requires=['requests', 'asyncio', 'matplotlib', 'pandas', 'numpy', 'polars', 'pytz']
