3b0f208600e1480c7df17571e0acc814b0c5ec59
lawnn <bloom.all.over.12@gmail.com>
2023-07-23 09:03:52 +0900

    引数を整える

--- fetcher/bybit.py
+++ fetcher/bybit.py
@@ -3,10 +3,12 @@ from datetime import datetime, timedelta
 from .util import pl_merge, make_ohlcv_from_timestamp
 
 
-def bybit_make_ohlcv(symbol: str, date: str, time_frame, pl_type: pl.PolarsDataType=pl.Float64) -> pl.DataFrame:
+def bybit_make_ohlcv(date: str, symbol: str = "BTCUSDT", time_frame: str = "1s",
+                     pl_type: pl.PolarsDataType=pl.Float64) -> pl.DataFrame:
     """
     example
-    bybit_make_ohlcv("BTCUSDT", "2023-05-26", "1s", pl.Float64)
+    import polars as pl
+    bybit_make_ohlcv("2023-05-26", "BTCUSDT", "1s", pl.Float64)
     """
     df = make_ohlcv_from_timestamp(f"https://public.bybit.com/trading/{symbol}/{symbol}{date}.csv.gz",
                                    "timestamp", "price", "size", "side", "Buy", "Sell", time_frame, pl_type, 1_000_000)
