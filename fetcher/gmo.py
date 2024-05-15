55488e4e15609305c03d528e882ccd3fac6a0140
lawnn <bloom.all.over.12@gmail.com>
2024-05-15 07:12:15 +0000

    FXのヒストリカルデータ取得

--- fetcher/gmo.py
+++ fetcher/gmo.py
@@ -213,3 +213,54 @@ def gmo_make_ohlcv(date: str, symbol: str = 'BTC_JPY',
     dt_range = pl.DataFrame({'datetime': pl.date_range(start_dt, end_dt, time_frame, eager=True)})
     return pl_merge(dt_range, df, "datetime")
 
+
+def gmo_FX_get_historical(start_ymd: str, end_ymd: str = None, symbol: str = 'USD_JPY', interval: str = '1min',
+                       output_dir: str = None, request_interval: float = 0.2, progress_info: bool = True) -> None:
+    """ example
+    gmo_get_historical('2021/09/01', '2021/09/08')
+    :param start_ymd: 2021/09/01
+    :param end_ymd: 2021/09/08
+    :param symbol: BTC, BTC_JPY, ETH_JPY
+    :param interval: 1min 5min 10min 15min 30min 1hour 4hour 8hour 12hour 1day 1week 1month
+    :param output_dir: csv/hoge/huga/
+    :param request_interval: 0
+    :param progress_info: False
+    :return:
+    """
+    if output_dir is None:
+        output_dir = f'./gmo_FX/{symbol}/ohlcv/{interval}'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    start_ymd = start_ymd.replace('/', '-')
+    if end_ymd is None:
+        end_ymd = start_ymd
+    else:
+        end_ymd = end_ymd.replace('/', '-')
+    start_dt = datetime.strptime(start_ymd, '%Y-%m-%d')
+    end_dt = datetime.strptime(end_ymd, '%Y-%m-%d')
+    if start_dt > end_dt:
+        raise ValueError(f'end_ymd{end_ymd} should be after start_ymd{start_ymd}.')
+
+    print(f'output dir: {output_dir}  save term: {start_dt:%Y/%m/%d} -> {end_dt:%Y/%m/%d}')
+
+    cur_dt = start_dt
+    total_count = 0
+    while cur_dt <= end_dt:
+        r = requests.get(f'https://forex-api.coin.z.com/public/v1/klines',
+                         params=dict(symbol=symbol, priceType="ASK", interval=interval, date=cur_dt.strftime('%Y%m%d'))).json()
+        df = (
+            pl.DataFrame(r["data"])
+            .rename({"openTime": "datetime"})
+            .with_columns([
+                pl.col("datetime").cast(pl.Int64).map(lambda x: x * 1_000).cast(pl.Datetime(time_unit='us'))
+            ])
+        )
+        df.write_csv(f'{output_dir}/{cur_dt.strftime("%Y-%m-%d")}.csv')
+        total_count += 1
+        if progress_info:
+            print(f'Completed output {cur_dt:%Y%m%d}.csv')
+
+        cur_dt += timedelta(days=1)
+        if request_interval > 0:
+            time.sleep(request_interval)
